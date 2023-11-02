9dbb51994b09f23373929ab3e65f7599e9ba3344
afiayeboah <afia.yakuamoah@gmail.com>
2023-11-02 13:55:31 -0700

    6!

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,27 +1,25 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-import sys
-from calculator_1 import add, sub, mul, div
-
-if len(sys.argv) != 4:
-    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-    exit(1)
-
-a, operator, b = sys.argv[1], sys.argv[2], sys.argv[3]
-
-if operator not in ('+', '-', '*', '/'):
-    print("Unknown operator. Available operators: +, -, *, and /")
-    exit(1)
-
-a, b = int(a), int(b)
-
-if operator == '+':
-    result = add(a, b)
-elif operator == '-':
-    result = sub(a, b)
-elif operator == '*':
-    result = mul(a, b)
-else:
-    result = div(a, b)
-
-print(f"{a} {operator} {b} = {result}")
+    import sys
+    from calculator_1 import add, sub, mul, div
+    args = sys.argv[1:]
+    arg_count = len(args)
+    valid_operators = ["+", "-", "*", "/"]
+    if arg_count != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    elif args[1] not in valid_operators:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    else:
+        num1 = int(args[0])
+        num2 = int(args[2])
+        operator = args[1]
+        if operator == "+":
+            print("{:d} + {:d} = {:d}".format(num1, num2, add(num1, num2)))
+        elif operator == "-":
+            print("{:d} - {:d} = {:d}".format(num1, num2, sub(num1, num2)))
+        elif operator == "*":
+            print("{:d} * {:d} = {:d}".format(num1, num2, mul(num1, num2)))
+        elif operator == "/":
+            print("{:d} / {:d} = {:d}".format(num1, num2, div(num1, num2)))
