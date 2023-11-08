70aec61253d5d750c6f32b4c33ea2db1d9b9a748
afiayeboah <afia.yakuamoah@gmail.com>
2023-11-08 12:03:06 -0800

    errors

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
- return list(map(lambda row: list(map(lambda element: element**2, row)), matrix))
+    return list(map(lambda row:
+                    list(map(lambda element: element**2, row)), matrix))
--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -2,4 +2,3 @@
 def simple_delete(a_dictionary, a_key=""):
     a_dictionary.pop(a_key, None)
     return a_dictionary
-
