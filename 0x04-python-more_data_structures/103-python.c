7b709f6a5e5c9699678c43b75db996aeafcdd5ef
afiayeboah <afia.yakuamoah@gmail.com>
2023-11-08 14:37:58 -0800

    103

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -3,37 +3,11 @@
 #include <listobject.h>
 #include <bytesobject.h>
 
-void print_python_bytes(PyObject *p)
-{
-    long int size;
-    int i;
-    char *data = NULL;
-
-    printf("[.] bytes object info\n");
-    if (!PyBytes_Check(bytes_obj))
-    {
-        printf("  [ERROR] Invalid Bytes Object\n");
-        return;
-    }
-
-    PyBytes_AsStringAndSize(bytes_obj, &data, &size);
-
-    printf("  size: %li\n", size);
-    printf("  data: %s\n", data);
-    if (size < 10)
-        printf("  first %li bytes:", size + 1);
-    else
-        printf("  first 10 bytes:");
-    for (i = 0; i <= size && i < 10; i++)
-        printf(" %02hhx", data[i]);
-    printf("\n");
-}
-
 void print_python_list(PyObject *p)
 {
-    long int size = PyList_Size(list_obj);
+    long int size = PyList_Size(p);
     int i;
-    PyListObject *list = (PyListObject *)list_obj;
+    PyListObject *list = (PyListObject *)p;
     const char *type;
 
     printf("[*] Python list info\n");
@@ -47,3 +21,4 @@ void print_python_list(PyObject *p)
             print_python_bytes(list->ob_item[i]);
     }
 }
+
