75b6370c8eb5f82e8da8fe7425478a3354fa26ae
afiayeboah <afia.yakuamoah@gmail.com>
2023-11-08 14:07:01 -0800

    13

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -3,21 +3,30 @@
 #include <listobject.h>
 #include <bytesobject.h>
 
-void print_python_bytes(PyObject *p)
-{
-    long int size = PyList_Size(p);
+void print_python_bytes(PyObject *p) {
+    long int byte_size;
     int i;
-    PyListObject *list = (PyListObject *)p;
-    const char *type;
+    char *byte_data = NULL;
 
-    printf("[*] Python list info\n");
-    printf("[*] Size of the Python List = %li\n", size);
-    printf("[*] Allocated = %li\n", list->allocated);
-    for (i = 0; i < size; i++)
-    {
-        type = (list->ob_item[i])->ob_type->tp_name;
-        printf("Element %i: %s\n", i, type);
-        if (!strcmp(type, "bytes"))
-            print_python_bytes(list->ob_item[i]);
+    printf("[.] Python Bytes Object Info\n");
+
+    if (!PyBytes_Check(p)) {
+        printf("  [ERROR] Invalid Bytes Object\n");
+        return;
     }
+
+    PyBytes_AsStringAndSize(p, &byte_data, &byte_size);
+
+    printf("  Size: %li\n", byte_size);
+    printf("  Data: %s\n", byte_data);
+
+    if (byte_size < 10)
+        printf("  First %li bytes:", byte_size + 1);
+    else
+        printf("  First 10 bytes:");
+
+    for (i = 0; i <= byte_size && i < 10; i++)
+        printf(" %02hhx", byte_data[i]);
+
+    printf("\n");
 }
