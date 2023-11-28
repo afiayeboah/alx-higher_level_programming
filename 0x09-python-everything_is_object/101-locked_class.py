4ff718c9ac3100bd4c6c1c57915e4ee8f7d571cb
afiayeboah <afia.yakuamoah@gmail.com>
2023-11-28 17:03:52 +0000

    pycode

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-""" LockedClass Module
-This module defines the `LockedClass` class 
-with restricted attributes using __slots__."""
+"""LockedClass Module"""
 
 
 class LockedClass():
-    """LockedClass with restricted attributes."""
+   """LockedClass with restricted attributes."""
     __slots__ = ('first_name',)
