673764693168d5f6317fe0f2894693683b1541d4
afiayeboah <afia.yakuamoah@gmail.com>
2023-12-04 12:38:34 -0800

    errors

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -7,5 +7,4 @@ class MyList(list):
 
     def print_sorted(self):
         """Print a sorted version of the list."""
-        sorted_list = sorted(self)
-        print(sorted_list)
+        print(sorted(self))
