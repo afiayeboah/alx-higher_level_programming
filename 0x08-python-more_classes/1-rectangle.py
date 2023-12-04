a0cefdada336d9867c1d1a3845e8bade43e9695b
afiayeboah <afia.yakuamoah@gmail.com>
2023-12-04 08:19:52 -0800

    read

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-    """Defines a re5ctangle with width and height attributes."""
+"""Defines a re5ctangle with width and height attributes."""
 
 
-    def __init__(self, width=0, height=0):
-        """Initializes a rectangle with optional width and height."""
+def __init__(self, width=0, height=0):
+    """Initializes a rectangle with optional width and height."""
         self.width = width
         self.height = height
 
