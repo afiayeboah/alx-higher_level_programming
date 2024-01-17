e65f890164cad84b310bb1eb575de6c6280239a7
afiayeboah <afia.yakuamoah@gmail.com>
2024-01-17 10:52:23 -0800

    12

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,8 @@
 -- List genres and the number of shows linked to each
-SELECT g.genre AS genre,
-       COUNT(s.id) AS number_of_shows
-FROM tv_show_genres AS g
-INNER JOIN tv_shows AS s ON g.show_id = s.id
-GROUP BY g.genre
+SELECT g.name AS genre,
+       COUNT(*) AS number_of_shows
+FROM tv_genres AS g
+INNER JOIN tv_shows_genres AS s
+ON g.id = s.genre_id
+GROUP BY g.name
 ORDER BY number_of_shows DESC;
