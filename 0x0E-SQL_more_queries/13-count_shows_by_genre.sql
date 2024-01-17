b88b37c2ad9282add8b6d4a8f0239ee34a226650
afiayeboah <afia.yakuamoah@gmail.com>
2024-01-17 11:16:51 -0800

    16

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,7 +2,7 @@
 SELECT g.name AS genre,
        COUNT(*) AS number_of_shows
 FROM tv_genres AS g
-INNER JOIN tv_shows_genres AS s
-ON g.id = s.genre_id
+INNER JOIN tv_shows_genre AS t
+ON g.id = t.genre_id
 GROUP BY g.name
 ORDER BY number_of_shows DESC;
