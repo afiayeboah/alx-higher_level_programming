0cd5830076ef58c4eccc1379238ecc3d019c4cef
afiayeboah <afia.yakuamoah@gmail.com>
2024-01-17 14:06:10 -0800

    error 20

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,5 +1,5 @@
 -- script that lists all genres in the database hbtn_0d_tvshows_rate by their rating.
-SELECT tg.name AS genre, COALESCE(SUM(rate), 0) AS rating
+SELECT tg.name AS name, COALESCE(SUM(rate), 0) AS rating
 FROM tv_genres AS tg
 LEFT JOIN tv_show_genres AS tsg
 ON tg.id = tsg.genre_id
