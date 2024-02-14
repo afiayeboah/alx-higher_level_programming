8bf573db64742f12bd72fb18723eea04472d356f
afiayeboah <afia.yakuamoah@gmail.com>
2024-02-14 18:06:39 +0000

    error

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,4 +1,7 @@
 #!/usr/bin/node
-const list = require('./100-data').list;
+const { list } = require('./100-data');
+
+const multipliedList = list.map((value, index) => value * index);
+
 console.log(list);
-console.log(list.map((x, i) => x * i));
+console.log(multipliedList);
