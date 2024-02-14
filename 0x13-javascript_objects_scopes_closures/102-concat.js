08ac669b114003ab2f7294657ef7901589a6cfb1
afiayeboah <afia.yakuamoah@gmail.com>
2024-02-14 19:54:09 +0000

    errors

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,6 +1,14 @@
 #!/usr/bin/node
-const fs = require('fs'):
 
-const fArg = fs. readFileSync(process.argv[2].toString();
-const sArg = fs. readFileSync(process.argv[3].toString();
-fs.writeFileSync(process.argv[4], fArg + sArg;
+const { readFileSync, writeFile } = require('fs');
+const { argv } = require('process');
+
+const getContent = (file) => readFileSync(file, 'utf8');
+
+const file1Content = getContent(argv[2]);
+const file2Content = getContent(argv[3]);
+const concatenatedContent = file1Content + '' + file2Content;
+
+writeFile(argv[4], concatenatedContent, 'utf8', (err) => {
+  if (err) throw err;
+});
