347ba263a6a01ab40bc3a6af21aa8c57ce7a7b0c
afiayeboah <afia.yakuamoah@gmail.com>
2024-02-20 10:33:23 -0800

    error

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -15,8 +15,8 @@ if __name__ == "__main__"
 
     cur = db.cursor()
 
-    cur.execute("SELECT * FROM states WHERE name=%s ORDER BY id ASC",
-                (sys.argv[4],))
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
+                .format(sys.argv[4]))
 
     rows = cur.fetchall()
 
