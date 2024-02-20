80ce5500e577a8c1ab580f20831918c86d54cbe7
afiayeboah <afia.yakuamoah@gmail.com>
2024-02-20 10:10:51 -0800

    13

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+""" Script that prints the State object with the name passed as argument
+from the database hbtn_0e_6_usa """
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+import sys
+
+if __name__ == "__main__":
+    username, password, database, state_name = sys.argv[1],
+    sys.argv[2], sys.argv[3], sys.argv[4]
+
+    if len(sys.argv) < 5 or ";" in state_name:
+        sys.exit(1)
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(username, password, database),
+                           pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    result = session.query(State).filter(State.name == state_name).first()
+
+    if result:
+        print(result.id)
+    else:
+        print("Not found")
+
+    session.close()
---
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+"""Script to list all State and corresponding City objects in the database."""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from relationship_city import City
+from relationship_state import Base, State
+
+
+if __name__ == "__main__":
+    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+    connection_str = "mysql+mysqldb://{}:{}@localhost:3306/{}"
+    engine = create_engine(connection_str.format(username, password, database))
+    Session = sessionmaker(bind=engine)
+
+    Base.metadata.create_all(engine)
+
+    session = Session()
+
+    states = session.query(State).order_by(State.id).all()
+
+    for state in states:
+        print("State {}: {}".format(state.id, state.name))
+        if state.cities:
+            for city in state.cities:
+                print("\tCity {}: {}".format(city.id, city.name))
+        else:
+            print("\tNo cities found for this state")
+
+    session.close()
