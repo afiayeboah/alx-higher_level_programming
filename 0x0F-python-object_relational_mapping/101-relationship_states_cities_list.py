98a07002a6ba0b6e05ace288cff65b8f9623faa3
afiayeboah <afia.yakuamoah@gmail.com>
2024-02-21 03:50:20 -0800

    102

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,22 +1,24 @@
 #!/usr/bin/python3
 
-""" Script to list all State and corresponding
-City objects in the database """
+"""Script to list all State and corresponding City objects in the database."""
+
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from relationship_city import City
 from relationship_state import Base, State
 
-
 if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
+    
     username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
     connection_str = "mysql+mysqldb://{}:{}@localhost:3306/{}"
     engine = create_engine(connection_str.format(username, password, database))
     Session = sessionmaker(bind=engine)
 
     Base.metadata.create_all(engine)
-
     session = Session()
 
     states = session.query(State).order_by(State.id).all()
--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" Script to list all City objects from the database hbtn_0e_101_usa """
+"""Script to list all City objects from the database hbtn_0e_101_usa"""
+
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -7,8 +8,11 @@ from relationship_city import Base, City
 from relationship_state import State
 
 if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
+    
     username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
-
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(username, password, database),
                            pool_pre_ping=True)
@@ -22,5 +26,5 @@ if __name__ == "__main__":
 
     for city in cities:
         print("{}: {} -> {}".format(city.id, city.name, city.state.name))
-
+        
     session.close()
